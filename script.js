b34e7fe660034de0db27820a03633489aa0ea975
Luis <hdluish@gmail.com>
2026-06-03 18:29:38 -0500

    Cambio de formatos jpg a webp, mejor transicion de banner Hero, nuevas imagenes añadidas

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ if (heroSlider) {
     const slides = heroSlider.querySelectorAll('.hero-slide');
     if (slides.length > 1) {
         let currentSlide = 0;
-        const slideDuration = 7000; // 7 segundos por imagen
+        const slideDuration = 6000; // 6 segundos por imagen
         
         function nextSlide() {
             slides[currentSlide].classList.remove('active');
